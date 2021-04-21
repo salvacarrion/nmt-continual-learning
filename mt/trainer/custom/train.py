db44264cb37dee26c3df8d80ee2ff585245f9023
salvacarrion <salva.carrion@outlook.com>
2021-04-21 02:37:26 +0200

    Incremental improvements

--- mt/trainer/custom/train.py
+++ mt/trainer/custom/train.py
@@ -29,6 +29,8 @@ BPE_FOLDER = "bpe.8000"
 
 MAX_EPOCHS = 1000
 LEARNING_RATE = 1e-4
+WARMUP_UPDATES = 4000
+PATIENCE = 5
 DEVICE1 = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 DEVICE2 = None  #torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -59,16 +61,18 @@ def run_experiment(datapath, src, trg, model_name, bpe_folder, domain=None, batc
     datasets = helpers.load_dataset(os.path.join(datapath, bpe_folder), src, trg, splits=["train", "val", "test"])
 
     # Prepare data loaders
-    train_loader = helpers.build_dataloader(datasets["train"], src_tok, trg_tok, batch_size=batch_size, max_tokens=max_tokens, num_workers=num_workers)
+    train_loader = helpers.build_dataloader(datasets["test"], src_tok, trg_tok, batch_size=batch_size, max_tokens=max_tokens, num_workers=num_workers)
     val_loader = helpers.build_dataloader(datasets["val"], src_tok, trg_tok, batch_size=batch_size, max_tokens=max_tokens, num_workers=num_workers, shuffle=False)
     # test_loader = helpers.build_dataloader(datasets["test"], src_tok, trg_tok, batch_size=batch_size, max_tokens=max_tokens, num_workers=num_workers, shuffle=False)
 
     # Instantiate model #1
     model1 = TransformerModel(src_tok=src_tok, trg_tok=trg_tok)
+    checkpoint_path = os.path.join(datapath, DATASET_CHECKPOINT_NAME, f"{model_name}_best.pt")
+    model1.load_state_dict(torch.load(checkpoint_path))
     model1.to(DEVICE1)
     optimizer1 = ScheduledOptim(
         optim.Adam(model1.parameters(), betas=(0.9, 0.98), eps=1e-09),
-        model1.d_model, 4000)
+        model1.d_model, WARMUP_UPDATES)
 
     # Set loss (ignore when the target token is <pad>)
     criterion = nn.CrossEntropyLoss(ignore_index=trg_tok.word2idx[trg_tok.PAD_WORD])
@@ -78,32 +82,40 @@ def run_experiment(datapath, src, trg, model_name, bpe_folder, domain=None, batc
     val_writer = SummaryWriter(os.path.join(datapath, DATASET_LOGS_NAME, f"{model_name}/val"))
 
     # Train and validate model
-    fit((model1, optimizer1), (None, None), train_loader, val_loader=val_loader,
+    fit((model1, optimizer1), (None, None), train_loader=train_loader, val_loader=val_loader,
         epochs=MAX_EPOCHS, criterion=criterion,
-        checkpoint_path=os.path.join(datapath, DATASET_CHECKPOINT_NAME, f"{model_name}_best.pt"), tr_writer=tr_writer, val_writer=val_writer)
+        checkpoint_path=checkpoint_path,
+        tr_writer=tr_writer, val_writer=val_writer)
 
     print("Done!")
 
 
 def fit(model_opt1, model_opt2, train_loader, val_loader, epochs, criterion, checkpoint_path, tr_writer=None, val_writer=None):
     lowest_val = 1e9
-    for epoch in range(epochs):
+    last_checkpoint = 0
+    for epoch_i in range(epochs):
         start_time = time.time()
-        n_iter = epoch + 1
 
         # Train model
-        tr_loss, tr_metrics = train(model_opt1, model_opt2, train_loader, criterion, epoch_i=n_iter, tb_writer=tr_writer)
+        #tr_loss, tr_metrics = train(model_opt1, model_opt2, train_loader, criterion, epoch_i=epoch_i, tb_writer=tr_writer)
 
         # Evaluate
-        val_loss, val_metrics = evaluate(model_opt1[0], val_loader, criterion, epoch_i=n_iter, tb_writer=val_writer)
+        val_loss, val_metrics = evaluate(model_opt1[0], val_loader, criterion, epoch_i=epoch_i, tb_writer=val_writer)
 
         # Save checkpoint
         if val_loss < lowest_val:
             avg_bleu = sum([x["torch_bleu"] for x in val_metrics]) / len(val_metrics)
             print(f"New best score! Loss={val_loss} | BLEU={avg_bleu}. (Saving checkpoint...)")
+            last_checkpoint = epoch_i
             lowest_val = val_loss
             torch.save(model_opt1[0].state_dict(), checkpoint_path)
             print("=> Checkpoint saved!")
+
+        else:
+            # Early stop
+            if (epoch_i-last_checkpoint) >= PATIENCE:
+                print(f"Early stop. Validation loss didn't improve for {PATIENCE} epochs")
+                break
 
 
 def gen_nopeek_mask(length):
@@ -165,10 +177,11 @@ def train(model_opt1, model_opt2, data_loader, criterion, clip=0.25, log_interva
     return total_loss / len(data_loader), all_metrics
 
 
-def evaluate(model, data_loader, criterion, log_interval=1, epoch_i=None, tb_writer=None):
+def evaluate(model, data_loader, criterion, log_interval=1, epoch_i=None, tb_writer=None, print_translations=True):
     total_loss = 0.0
     all_metrics = []
     start_time = time.time()
+    src_dec_all, ref_dec_all, hyp_dec_all = [], [], []
 
     model.eval()
     for i, batch in enumerate(data_loader):
@@ -195,14 +208,20 @@ def evaluate(model, data_loader, criterion, log_interval=1, epoch_i=None, tb_wri
             # Log progress
             if (i+1) % log_interval == 0:
                 # Get translations
-                src_dec, ref_dec, hyp_dec = get_translations(outputs.detach(), src, trg, model.src_tok, model.trg_tok)
-
-                # Print translations
-                helpers.print_translations(hypothesis=hyp_dec, references=ref_dec, source=src_dec, limit=1)
+                src_dec, ref_dec, hyp_dec = get_translations(src, trg, torch.argmax(outputs.detach(), dim=-1), model.src_tok, model.trg_tok)
+                if print_translations:
+                    src_dec_all += src_dec
+                    ref_dec_all += ref_dec
+                    hyp_dec_all += hyp_dec
 
                 # Log progress
                 metrics = log_progress("val", total_loss, epoch_i+1, i+1, len(data_loader), start_time, tb_writer, translations=(src_dec, ref_dec, hyp_dec))
                 all_metrics.append(metrics)
+
+    # Print translations
+    if print_translations:
+        helpers.print_translations(hypothesis=hyp_dec_all, references=ref_dec_all, source=src_dec_all, limit=50)
+
     return total_loss / len(data_loader), all_metrics
 
 
@@ -245,10 +264,7 @@ def log_progress(prefix, total_loss, epoch_i, batch_i, n_batches, start_time, tb
     return metrics
 
 
-def get_translations(outputs, src, trg, src_tok, trg_tok):
-    src_vocab, trg_vocab = src_tok.get_vocab_size(), trg_tok.get_vocab_size()
-    trg_pred = torch.argmax(outputs.view(outputs.shape[0], -1, trg_vocab), dim=2)  # B, L, E
-
+def get_translations(src, trg, trg_pred, src_tok, trg_tok):
     # Decode tensors
     src_dec = src_tok.decode(src)
     ref_dec = trg_tok.decode(trg)
