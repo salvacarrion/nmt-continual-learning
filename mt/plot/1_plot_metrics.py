06cba5e799adb0a0f0807f4411e234b394504e1a
salvacarrion <salva.carrion@outlook.com>
2021-06-03 13:44:46 +0200

    Minor improvements

--- mt/plot/1_plot_metrics.py
+++ mt/plot/1_plot_metrics.py
@@ -16,7 +16,7 @@ sns.set()
 
 
 
-def get_metrics(datapath, src, trg, model_name, train_domain):
+def get_metrics(datapath, src, trg, model_name, label, train_domain):
     metrics = []
     # Get all folders in the root path
     for test_domain in ["health", "biological", "merged"]:
@@ -24,13 +24,13 @@ def get_metrics(datapath, src, trg, model_name, train_domain):
         eval_path = os.path.join(datapath, DATASET_EVAL_NAME, model_name, test_domain)
         with open(os.path.join(eval_path, 'metrics.json'), 'r') as f:
             # Load metrics json
-            row = json.load(f)["val"]
-
-            # Prettify name
-            pretty_model_name = model_name.replace("transformer_", "").replace("_best.pt", "").replace("_", " ").strip()  # remove ending ".pt"
+            json_metrics = json.load(f)
+            key = [k for k in list(json_metrics.keys()) if "beam" in k][0]  # "val"
+            row = json_metrics[key]
 
             # Add more data
-            row["model_name"] = pretty_model_name
+            row["model_name"] = model_name
+            row["label"] = label
             row["train_domain"] = train_domain.title()
             row["test_domain"] = test_domain.title()
             row["lang"] = f"{src}-{trg}"
@@ -45,7 +45,7 @@ def plot_metrics(df_metrics, savepath, lang_pair, metric=("sacrebleu_bleu", "ble
     df_lang = df_metrics[df_metrics.lang == lang_pair]
 
     # Draw a nested barplot by species and sex
-    g = sns.catplot(data=df_lang, x="model_name", y=metric_id, kind="bar", hue="test_domain", legend=False)
+    g = sns.catplot(data=df_lang, x="label", y=metric_id, kind="bar", hue="test_domain", legend=False)
     g.fig.set_size_inches(12, 8)
 
     # properties
@@ -72,20 +72,34 @@ if __name__ == "__main__":
     lang_pair = "es-en"
     metric = ("sacrebleu_bleu", "bleu")  # (ID, pretty name)
     datasets = [(os.path.join(DATASETS_PATH, x), l) for x, l in [
-        ("health_es-en", ["transformer_health_best.pt"]),
-        ("biological_es-en", ["transformer_biological_best.pt"]),
-        ("merged_es-en", ["transformer_merged_best.pt"]),
-        ("health_biological_inter_es-en", ["transformer_health_biological_inter_a0.0_best.pt", "transformer_health_biological_inter_a0.25_best.pt", "transformer_health_biological_inter_a0.5_best.pt", "transformer_health_biological_inter_a0.75_best.pt"]),
-        ("health_biological_lwf_es-en", ["transformer_health_biological_lwf_a0.25_best.pt", "transformer_health_biological_lwf_a0.5_best.pt", "transformer_health_biological_lwf_a0.75_best.pt"]),
+        ("health_fairseq_es-en", [("checkpoint_best.pt", "Health (Fairseq)")]),
+        ("biological_fairseq_es-en", [("checkpoint_best.pt", "Biological (Fairseq)")]),
+        ("merged_fairseq_es-en", [("checkpoint_best.pt", "H+B (Fairseq)")]),
+        ("health_biological_fairseq_es-en", [("checkpoint_best.pt", "H→B (Fairseq)")]),
+        ("health_es-en", [("transformer_health_best.pt", "Health (Custom)")]),
+        ("biological_es-en", [("transformer_biological_best.pt", "Biological (Custom)")]),
+        ("merged_es-en", [("transformer_merged_best.pt", "H+B (Custom)")]),
+        ("health_biological_inter_es-en", [
+            ("transformer_health_biological_inter_a0.0_best.pt", "H→B (Inter; a=0.0)"),
+            ("transformer_health_biological_inter_a0.25_best.pt", "H→B (Inter; a=0.25)"),
+            ("transformer_health_biological_inter_a0.5_best.pt", "H→B (Inter; a=0.50)"),
+            ("transformer_health_biological_inter_a0.75_best.pt", "H→B (Inter; a=0.75)"),
+            ]
+         ),
+        ("health_biological_lwf_es-en", [
+            ("transformer_health_biological_lwf_a0.25_best.pt", "H→B (LwF; a=0.25)"),
+            ("transformer_health_biological_lwf_a0.5_best.pt", "H→B (LwF; a=0.50)"),
+            ("transformer_health_biological_lwf_a0.75_best.pt", "H→B (LwF; a=0.75)"),
+        ]),
     ]]
     for dataset, models in datasets:
         domain, (src, trg) = utils.get_dataset_ids(dataset)
         fname_base = f"{domain}_{src}-{trg}"
 
         # Train model
-        for model_name in models:
+        for model_name, label in models:
             print(f"Getting model ({fname_base}; {model_name})...")
-            metrics += get_metrics(dataset, src, trg, model_name=model_name, train_domain=domain)
+            metrics += get_metrics(dataset, src, trg, model_name=model_name, label=label, train_domain=domain)
 
     # Create folder
     summary_path = os.path.join(DATASETS_PATH, DATASET_SUMMARY_NAME)
@@ -99,4 +113,4 @@ if __name__ == "__main__":
 
     # Plot metrics
     plot_metrics(df, savepath=summary_path, lang_pair=lang_pair, metric=metric)
-    adsd = 3
+    print("Done!")
--- mt/trainer/fairseq/5_generate.py
+++ mt/trainer/fairseq/5_generate.py
@@ -5,6 +5,7 @@ import random
 import time
 from pathlib import Path
 import json
+import re
 
 from mt import utils
 from mt import DATASETS_PATH, DATASET_EVAL_NAME, DATASET_CLEAN_SORTED_NAME, DATASET_TOK_NAME, DATASET_LOGS_NAME, DATASET_CHECKPOINT_NAME
@@ -13,7 +14,7 @@ TOK_MODEL = "bpe"
 TOK_SIZE = 16000
 TOK_FOLDER = f"{TOK_MODEL}.{TOK_SIZE}"
 DOMAINS = ["health", "biological", "merged"]
-
+GET_SCORES = True
 
 def generate(train_dataset, src, trg, model_name, train_domain):
     # Get all folders in the root path
@@ -29,12 +30,12 @@ def generate(train_dataset, src, trg, model_name, train_domain):
         eval_path = os.path.join(train_dataset, DATASET_EVAL_NAME, model_name, test_domain)
         Path(eval_path).mkdir(parents=True, exist_ok=True)
 
-        # # Preprocess domain datasets with train tokenizers
-        # source_dataset = test_dataset
-        # vocab_path = train_dataset
-        # output_path = eval_path
-        # print(f"\t- Preprocessing datasets for: {test_domain}...")
-        # subprocess.call(['sh', './scripts/3_preprocess.sh', source_dataset, vocab_path, output_path, TOK_FOLDER, src, trg])
+        # Preprocess domain datasets with train tokenizers
+        source_dataset = test_dataset
+        vocab_path = train_dataset
+        output_path = eval_path
+        print(f"\t- Preprocessing datasets for: {test_domain}...")
+        subprocess.call(['sh', './scripts/3_preprocess.sh', source_dataset, vocab_path, output_path, TOK_FOLDER, src, trg])
 
         # Generate them
         eval_path_bin = os.path.join(eval_path, "data-bin")
@@ -53,21 +54,52 @@ def generate(train_dataset, src, trg, model_name, train_domain):
     print("")
 
 
+def get_scores(train_dataset, src, trg, model_name, train_domain):
+    # Get all folders in the root path
+    test_datasets = [os.path.join(DATASETS_PATH, x) for x in [f"health_{src}-{trg}",
+                                                              f"biological_{src}-{trg}",
+                                                              f"merged_{src}-{trg}"]]
+    for test_dataset in test_datasets:
+        test_domain, (test_src, test_trg) = utils.get_dataset_ids(test_dataset)
+        print(f"=> TESTING MODEL FROM '{train_domain}' IN DOMAIN '{test_domain}'")
+
+        # Create path
+        eval_path = os.path.join(train_dataset, DATASET_EVAL_NAME, model_name, test_domain)
+
+        # Read file
+        with open(os.path.join(eval_path, "generate-test.txt"), 'r') as f:
+            score_summary = f.readlines()[-1]
+            print(score_summary)
+
+        # Parse metrics
+        pattern = r"beam=(\d+): BLEU\d+ = (\d+.\d+)"
+        beam_width, score_bleu = re.search(pattern, score_summary).groups()
+        beam_width, score_bleu = int(beam_width), float(score_bleu)
+        metrics = {f"beam{beam_width}": {'sacrebleu_bleu': score_bleu}}
+
+        # Save metrics to file
+        with open(os.path.join(eval_path, 'metrics.json'), 'w') as f:
+            json.dump(metrics, f)
+        # print("Metrics saved!")
+    print("------------------------------------------------------------------------")
+
+
 if __name__ == "__main__":
     # Get all folders in the root path
     # datasets = [os.path.join(DATASETS_PATH, x) for x in ["health_es-en", "biological_es-en", "merged_es-en"]]
     datasets = [(os.path.join(DATASETS_PATH, x), l) for x, l in [
-        # ("health_fairseq_es-en", ["checkpoint_best.pt"]),
+        ("health_fairseq_es-en", ["checkpoint_best.pt"]),
         ("biological_fairseq_es-en", ["checkpoint_best.pt"]),
         ("merged_fairseq_es-en", ["checkpoint_best.pt"]),
+        ("health_biological_fairseq_es-en", ["checkpoint_best.pt"]),
       ]]
 
     for dataset, models in datasets:
         domain, (src, trg) = utils.get_dataset_ids(dataset)
         fname_base = f"{domain}_{src}-{trg}"
 
-
         # Train model
         for model_name in models:
             print(f"Testing model ({fname_base}; {model_name})...")
-            generate(dataset, src, trg, model_name=model_name, train_domain=domain)
+            func = get_scores if GET_SCORES else generate
+            func(dataset, src, trg, model_name=model_name, train_domain=domain)
